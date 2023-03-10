fa23bd60ddadfb3a620f12649fd1b94496e1759b
longbuii <105617872+longbuii@users.noreply.github.com>
2023-03-10 07:38:46 +0700

    Add project files.

---
+++ IdeaCool/Controllers/SubmissionsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using IdeaCool.Models;
+
+namespace IdeaCool.Controllers
+{
+    public class SubmissionsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Submissions
+        public ActionResult Index()
+        {
+            return View(db.Submissions.ToList());
+        }
+
+        // GET: Submissions/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Submission submission = db.Submissions.Find(id);
+            if (submission == null)
+            {
+                return HttpNotFound();
+            }
+            return View(submission);
+        }
+
+        // GET: Submissions/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Submissions/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Name,Deadline1,Deadline2")] Submission submission)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Submissions.Add(submission);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(submission);
+        }
+
+        // GET: Submissions/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Submission submission = db.Submissions.Find(id);
+            if (submission == null)
+            {
+                return HttpNotFound();
+            }
+            return View(submission);
+        }
+
+        // POST: Submissions/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Name,Deadline1,Deadline2")] Submission submission)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(submission).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(submission);
+        }
+
+        // GET: Submissions/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Submission submission = db.Submissions.Find(id);
+            if (submission == null)
+            {
+                return HttpNotFound();
+            }
+            return View(submission);
+        }
+
+        // POST: Submissions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Submission submission = db.Submissions.Find(id);
+            db.Submissions.Remove(submission);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ IdeaCool/Models/Submission.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace IdeaCool.Models
+{
+    public class Submission
+    {
+        //Create to variable for Submission
+        // You need to create the SubmissionControler.
+
+        [Key]
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public DateTime Deadline1 { get; set; }
+        [Required]
+        public DateTime Deadline2 { get; set; }
+
+
+    }
+}
